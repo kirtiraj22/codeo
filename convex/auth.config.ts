b383b6a67080e612d8fbd4ef66dfbf740c5b246f
kirtiraj22 <kirtirajthakor064@gmail.com>
2025-01-01 11:57:53 +0530

    clerk-convex auth webhook handler

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,9 +1,9 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    providers: [
-        {
-            domain: "https://comic-hog-97.clerk.accounts.dev/",
-            applicationID: "convex"
-        }
-    ]
-}
+	providers: [
+		{
+			domain: "https://comic-hog-97.clerk.accounts.dev/",
+			applicationID: "convex",
+		},
+	],
+};

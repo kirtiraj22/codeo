90f0f62acdeca6c9ae35af5073b3155853f913e5
kirtiraj22 <kirtirajthakor064@gmail.com>
2025-01-01 10:07:40 +0530

    convex provider(with clerk)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,8 @@
-import {
-	SignedIn,
-	SignedOut,
-	SignOutButton,
-	SignUpButton,
-} from "@clerk/nextjs";
 
 export default function Home() {
 	return (
 		<div>
-			<SignedOut>
-				<SignUpButton />
-			</SignedOut>
-
-			<SignedIn>
-				<SignOutButton />
-			</SignedIn>
+      hello
 		</div>
 	);
 }

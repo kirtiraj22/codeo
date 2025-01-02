7c29231c23b5389053604beb032da45428fda602
kirtiraj22 <kirtirajthakor064@gmail.com>
2025-01-02 22:23:34 +0530

    added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { SignInButton, SignOutButton } from "@clerk/nextjs";
 
 export default function Home() {
 	return (
 		<div>
-      hello
+			<SignInButton></SignInButton>
+			<SignOutButton></SignOutButton>
 		</div>
 	);
 }

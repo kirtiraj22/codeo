5421f25eb2ef4380d7f8b055796bcd00916d74f0
kirtiraj22 <kirtirajthakor064@gmail.com>
2025-01-07 17:01:58 +0530

    output panel

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
 			<div className="max-w-[1800px] mx-auto p-4">
 				<Header />
 				<div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-					<EditorPanel />
 					<OutputPanel />
+					<EditorPanel />
 				</div>
 			</div>
 		</div>

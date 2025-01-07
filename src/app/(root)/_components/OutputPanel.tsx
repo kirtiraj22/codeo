878ed60b740053a51419b7bdfcea61643f33bcd1
kirtiraj22 <kirtirajthakor064@gmail.com>
2025-01-07 17:36:35 +0530

    running state skeleton

--- src/app/(root)/_components/OutputPanel.tsx
+++ src/app/(root)/_components/OutputPanel.tsx
@@ -3,6 +3,7 @@ import { useCodeEditorStore } from "@/store/useCodeEditorStore";
 import { AlertTriangle, CheckCircle, Clock, Copy, Terminal } from "lucide-react";
 
 import { useState } from "react";
+import RunningCodeSkeleton from "./RunningCodeSkeleton";
 
 function OutputPanel() {
 	const { output, error, isRunning } = useCodeEditorStore();
@@ -11,9 +12,17 @@ function OutputPanel() {
 	const hasContent = true;
 	// const hasContent = error || output;
 
+  const handleCopy = async () => {
+    if(!hasContent) return;
+    await navigator.clipboard.writeText(error || output);
+    setIsCopied(true);
+
+    setTimeout(() => setIsCopied(false), 2000)
+  }
+
 	return (
-		<div>
-			<div>
+		<div className="relative bg-[#181825] rounded-xl p-4 ring-1 ring-gray-800/50">
+			<div className="flex items-center justify-between mb-3">
 				<div className="flex items-center gap-2">
 					<div className="flex items-center justify-center w-6 h-6 rounded-lg bg-[#1e1e2e] ring-1 ring-gray-800/50">
 						<Terminal className="w-4 h-4 text-blue-400" />
@@ -23,11 +32,11 @@ function OutputPanel() {
 					</span>
 				</div>
 				{hasContent && (
-					<button>
+					<button onClick={handleCopy} className="flex items-center gap-1.5 px-2.5 py-1.5 text-xs text-gray-400 hover:text-gray-300 bg-[#1e1e2e] rounded-lg ring-1 ring-gray-800/50 hover:ring-gray-700/50 transition-all">
 						{isCopied ? (
 							<>
 								<CheckCircle className="w-3.5 h-3.5" />
-								Copied
+								Copied!
 							</>
 						) : (
 							<>
@@ -42,7 +51,7 @@ function OutputPanel() {
 			<div className="relative">
 				<div className="relative bg-[#1e1e2e]/50 backdrop-blur-sm border border-[#313244] rounded-xl p-4 h-[600px] overflow-auto font-mono text-sm">
 					{isRunning ? (
-						<div>Running </div>
+            <RunningCodeSkeleton />
 					) : error ? (
             <div className="flex items-start gap-3 text-red-400">
               <AlertTriangle className="w-5 h-5 flex-shrink-0 mt-1"/>
@@ -64,7 +73,7 @@ function OutputPanel() {
               <div className="flex items-center justify-center w-12 h-12 rounded-xl bg-gray-800/50 ring-1 ring-gray-700/50 mb-4">
                 <Clock className="w-6 h-6"/>
               </div>
-							<p>Run your code to see the output here...</p>
+							<p className="text-center">Run your code to see the output here...</p>
 						</div>
 					)}
 				</div>

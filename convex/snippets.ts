dc8111cbb67b3f87ba2f8072f974819aa60971cd
kirtiraj22 <kirtirajthakor064@gmail.com>
2025-01-18 12:24:56 +0530

    snippet detail page

--- convex/snippets.ts
+++ convex/snippets.ts
@@ -160,20 +160,25 @@ export const getStarredSnippets = query({
 });
 
 export const getSnippetById = query({
-	args: { snippetId: v.id("snippets")},
+	args: { snippetId: v.id("snippets") },
 	handler: async (ctx, args) => {
 		const snippet = await ctx.db.get(args.snippetId);
-		if(!snippet) throw new Error("Snippet not found!")
+		if (!snippet) throw new Error("Snippet not found!");
 
 		return snippet;
-	}
-})
+	},
+});
 
 export const getComments = query({
 	args: { snippetId: v.id("snippets") },
 	handler: async (ctx, args) => {
-		const comments = await ctx.db.query("snippetComments").withIndex("by_snippet_id").filter((q) => q.eq(q.field("snippetId"), args.snippetId)).order("desc").collect();
+		const comments = await ctx.db
+			.query("snippetComments")
+			.withIndex("by_snippet_id")
+			.filter((q) => q.eq(q.field("snippetId"), args.snippetId))
+			.order("desc")
+			.collect();
 
 		return comments;
-	}
-})
+	},
+});

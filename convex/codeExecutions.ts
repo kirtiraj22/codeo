067d706fa9f3688c7d7a85f136efb36f2b503e48
kirtiraj22 <kirtirajthakor064@gmail.com>
2025-01-19 23:26:47 +0530

    query for user's execution and snippet stats

--- convex/codeExecutions.ts
+++ convex/codeExecutions.ts
@@ -1,4 +1,5 @@
-import { mutation } from "./_generated/server";
+import { paginationOptsValidator } from "convex/server";
+import { mutation, query } from "./_generated/server";
 
 import { ConvexError, v } from "convex/values";
 
@@ -20,14 +21,101 @@ export const saveExecution = mutation({
 			.withIndex("by_user_id")
 			.filter((q) => q.eq(q.field("userId"), identity.subject))
 			.first();
-        
-        if(!user?.isPro && args.language !== "javascript"){
-            throw new ConvexError("Pro subscription required to use this language")
-        }
 
-        await ctx.db.insert("codeExecutions", {
-            ...args,
-            userId: identity.subject
-        })
+		if (!user?.isPro && args.language !== "javascript") {
+			throw new ConvexError(
+				"Pro subscription required to use this language"
+			);
+		}
+
+		await ctx.db.insert("codeExecutions", {
+			...args,
+			userId: identity.subject,
+		});
+	},
+});
+
+export const getUserExecutions = query({
+	args: {
+		userId: v.string(),
+		paginationOpts: paginationOptsValidator,
+	},
+	handler: async (ctx, args) => {
+		return await ctx.db
+			.query("codeExecutions")
+			.withIndex("by_user_id")
+			.filter((q) => q.eq(q.field("userId"), args.userId))
+			.order("desc")
+			.paginate(args.paginationOpts);
+	},
+});
+
+export const getUserStats = query({
+	args: { userId: v.string() },
+	handler: async (ctx, args) => {
+		const executions = await ctx.db
+			.query("codeExecutions")
+			.withIndex("by_user_id")
+			.filter((q) => q.eq(q.field("userId"), args.userId))
+			.collect();
+
+		const starredSnippets = await ctx.db
+			.query("stars")
+			.withIndex("by_user_id")
+			.filter((q) => q.eq(q.field("userId"), args.userId))
+			.collect();
+
+		// snippet details to analyze language
+		const snippetIds = starredSnippets.map((star) => star.snippetId);
+		const snippetDetails = await Promise.all(
+			snippetIds.map((id) => ctx.db.get(id))
+		);
+
+		// most starred language
+		const starredLanguages = snippetDetails.filter(Boolean).reduce(
+			(acc, curr) => {
+				if (curr?.language) {
+					acc[curr.language] = (acc[curr.language] || 0) + 1;
+				}
+
+				return acc;
+			},
+			{} as Record<string, number>
+		);
+
+		const mostStarredLanguage =
+			Object.entries(starredLanguages).sort(
+				([, a], [, b]) => b - a
+			)[0]?.[0] ?? "N/A";
+
+		const last24Hours = executions.filter(
+			(e) => e._creationTime > Date.now() - 24 * 60 * 60 * 1000
+		).length;
+
+		const languageStats = executions.reduce(
+			(acc, curr) => {
+				acc[curr.language] = (acc[curr.language] || 0) + 1;
+				return acc;
+			},
+			{} as Record<string, number>
+		);
+
+		const languages = Object.keys(languageStats);
+
+		const favoriteLanguage = languages.length
+			? languages.reduce((a, b) =>
+					languageStats[a] > languageStats[b] ? a : b
+				)
+			: "N/A";
+
+		return {
+			totalExecutions: executions.length,
+			lagnuagesCount: languages.length,
+			languages: languages,
+			last24Hours,
+			favoriteLanguage,
+			languageStats,
+			mostStarredLanguage,
+		};
 	},
 });
